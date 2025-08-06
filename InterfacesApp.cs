aebe9ad630c5467635b6611d8e148080b62698e5
DanDDTQ <ddktetteh-quarcoo@st.ug.edu.gh>
2025-08-06 19:55:17 +0000

    AbstractClassesApp

---
+++ InterfacesApp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace InterfacesApp
+{
+    // 1. Interface
+    interface IMovable
+    {
+        void Move();
+    }
+
+    // 2. Implementers
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMovable car = new Car();
+            IMovable bike = new Bicycle();
+
+            car.Move();   // prints "Car is moving"
+            bike.Move();  // prints "Bicycle is moving"
+        }
+    }
+}
